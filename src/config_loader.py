06d4431c5a700a06354568eee6ab1b8fd4ff6b2d
anapdoliveira94 <anapdoliveira.94@hotmail.com>
2026-01-25 15:48:26 -0300

    Correcting the config_loader.py file

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,5 +1,5 @@
+from pathlib import Path
 import yaml
-import os
 
 
 def load_config(config_path: str) -> dict:
@@ -12,16 +12,25 @@ def load_config(config_path: str) -> dict:
     Returns:
         dict: Parâmetros de configuração.
     """
-    if not os.path.exists(config_path):
+    config_file = Path(config_path)
+
+    if not config_file.exists(config_path):
         raise FileNotFoundError(f"Arquivo de configuração não encontrado: {config_path}")
 
-    with open(config_path, "r") as file:
+    with config_file.open("r", encoding="utf-8") as file:
         config = yaml.safe_load(file)
 
     required_keys = ["image_1", "image_2", "output_dir", "threshold"]
-
     for key in required_keys:
         if key not in config:
             raise KeyError(f"Chave de configurção ausente: {key}")
+
+    #Converte paths to Path 
+    config["image_1"] = Path(config["image_1"])
+    config["image_2"] = Path(config["image_2"])
+    config["output_dir"] = Path(config["output_dir"])
+
+    #Cria o repositório output caso não exista
+    config["output_dir"].mkdir(parents=True, exist_ok=True)
 
     return config
